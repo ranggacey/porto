7d47b793596f309d2c4e91dd1558a1d9c718ab7d
ranggacey <118665954+ranggacey@users.noreply.github.com>
2024-08-17 20:37:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
+function toggleMode() {
+    document.body.classList.toggle('dark-mode');
+}
